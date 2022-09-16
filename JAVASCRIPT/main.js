5b6b9dd4b4eb17fb0b9c08f92d1f8120317904e8
Julius Khamara <khamarajulius@gmail.com>
2022-09-15 22:38:23 -0700

    Assignment

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,20 @@
+window.alert("Hello Users");
+
+document.getElementById("JK").innerHTML = 9 + 10;
+
+document.write("we are programmers");
+
+console.log("JK")
+
+console.log(5)
+
+var a,b,c,d;
+a = 10;
+b = 30;
+c = a+b;
+
+console.log(c);
+
+d = c+b;
+
+console.log(d);
